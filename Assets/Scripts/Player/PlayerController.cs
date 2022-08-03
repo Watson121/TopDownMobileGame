2af494f307892113069c894a8b1bc6400215ec4e
Watson121 <adamtedders@outlook.com>
2022-08-03 15:15:14 +0100

    Player Controller and camera updates
    Updated the Player Controller so that it will now rotate, and the player character is now clamp within the camera view.
    
    Camera angle moved to looking behind the player character.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,12 +6,25 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
+    // Player Control Inputs
     private PlayerInput playerInput;
     private PlayerControls playerControls;
 
     private Vector3 playerPostion;
-    private float playerSpeed = 10.0f;
+    private Vector3 playerRotation;
+    private float positionOffset = 2.0f;    
+
+
+    private float playerSpeed = 5f;
+    private float playerRotationSpeed = 10.0f;
+    private float horitontalRotation = 15.0f;
+    private float verticalRotation = 15.0f;
+
+
     private bool move = false;
+
+    
+
 
     private Vector2 screenBounds;
 
@@ -20,7 +33,7 @@ public class PlayerController : MonoBehaviour
         playerInput = new PlayerInput();
         playerControls = new PlayerControls();
         playerControls.Enable();
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Screen.width));
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     private void OnEnable()
@@ -60,10 +73,18 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 inputVector = playerControls.Player.Movement.ReadValue<Vector2>();
 
-        playerPostion += new Vector3(inputVector.x * playerSpeed * Time.deltaTime, 0, inputVector.y * playerSpeed * Time.deltaTime);
-        playerPostion.x = Mathf.Clamp(playerPostion.x , screenBounds.x, screenBounds.x * -1);
-        Debug.Log(Screen.width);
+        // Setting Player Position
+        playerPostion += new Vector3(inputVector.x * playerSpeed * Time.deltaTime, inputVector.y * playerSpeed * Time.deltaTime, 0);
+        playerPostion.x = Mathf.Clamp(playerPostion.x , (screenBounds.x + positionOffset), (screenBounds.x + positionOffset) * -1);
         
+        // Setting Player Rotation
+        playerRotation += new Vector3(0, inputVector.y * 10f * Time.deltaTime, inputVector.x * 10f * Time.deltaTime);
+        
+
         transform.position = playerPostion;
+        transform.rotation = Quaternion.Euler(playerRotation);
+
+        
+        
     }
 }
