9b187f43dc566e92a117f7d184d4409fa30a8abe
Watson121 <adamtedders@outlook.com>
2022-08-07 17:35:04 +0100

    Created a Check Health Task, this task checks if the current enemy has any health or not. If the enemy doesn't have any health then it will stop the enemy from moving. This will be updated to remove them from the screen.

--- Assets/Scripts/AI/CroutonShip.cs
+++ Assets/Scripts/AI/CroutonShip.cs
@@ -2,18 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using BehaviourTree;
 
-public class CroutonShip : Tree
+public class CroutonShip : Tree, IDamage
 {
-    public static float moveSpeed = 10.0f;
+    public static float moveSpeed = 2.0f;
     public static float fireSpeed = 3.0f;
     public static float damage = 5.0f;
 
+    public float Health
+    {
+        get { return health; }
+    }
+
+
+    [UnityEngine.SerializeField] private const float MAX_HEALTH = 10.0f;
+    [UnityEngine.SerializeField] private float health;
+
+    public void ApplyDamage(float damage)
+    {
+
+        UnityEngine.Debug.Log("Enemy Hit");
+
+        health -= damage;
+
+        health = UnityEngine.Mathf.Clamp(health, 0, MAX_HEALTH);
+    }
+
     protected override Node SetupTree()
     {
+        health = MAX_HEALTH;
+
         Node root = new Selector(new List<Node> 
         { 
             new Sequence(new List<Node>
             {
+                new CheckHealth(this),
                 new CheckIfInView(transform),
                 new Task_MoveAndFire(transform),
             }),
@@ -21,4 +43,6 @@ public class CroutonShip : Tree
 
         return root;
     }
+
+    
 }
